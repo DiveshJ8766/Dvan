ffacda7654103c535e64afb48396da55657125e4
Divesh Jadhav <diveshvjadhav@gmail.com>
2022-11-25 03:41:08 +0530

    Add files via upload

---
+++ context/DvanContext.js
@@ -0,0 +1,72 @@
+import { createContext, useEffect, useState } from "react";
+import { useRouter } from "next/router";
+
+export const DvanContext = createContext();
+
+export const DvanProvider = ({ children }) => {
+  const [appStatus, setAppStatus] = useState("");
+  const [currentAccount, setCurrentAccount] = useState("");
+  const router = useRouter();
+
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, []);
+
+  /*
+   * Checks if there is an active wallet connection
+   */
+  const checkIfWalletIsConnected = async () => {
+    if (!window.ethereum) return setAppStatus("noMetaMask");
+    try {
+      const addressArray = await window.ethereum.request({
+        method: "eth_accounts",
+      });
+      if (addressArray.length > 0) {
+        setAppStatus("connected");
+        setCurrentAccount(addressArray[0]);
+      } else {
+        router.push("/");
+        setAppStatus("notConnected");
+      }
+    } catch (err) {
+      router.push("/");
+      setAppStatus("error");
+    }
+  };
+
+  /*
+   * Initiates MetaMask wallet connection
+   */
+  const connectWallet = async () => {
+    if (!window.ethereum) return setAppStatus("noMetaMask");
+    try {
+      setAppStatus("loading");
+
+      const addressArray = await window.ethereum.request({
+        method: "eth_requestAccounts",
+      });
+
+      if (addressArray.length > 0) {
+        setAppStatus("connected");
+        setCurrentAccount(addressArray[0]);
+      } else {
+        router.push("/");
+        setAppStatus("notConnected");
+      }
+    } catch (err) {
+      setAppStatus("error");
+    }
+  };
+
+  return (
+    <DvanContext.Provider
+      value={{
+        appStatus,
+        currentAccount,
+        connectWallet,
+      }}
+    >
+      {children}
+    </DvanContext.Provider>
+  );
+};
