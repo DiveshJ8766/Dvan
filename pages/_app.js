f27aa6b06c066b1480620e9abd1877cfd9c2b34b
Divesh Jadhav <diveshvjadhav@gmail.com>
2022-11-25 03:41:35 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
 import "../styles/globals.css";
 
+import { DvanProvider } from "../context/DvanContext";
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <DvanProvider>
+      <Component {...pageProps} />
+    </DvanProvider>
+  );
 }
 
 export default MyApp;
