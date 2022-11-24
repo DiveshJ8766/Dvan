67157b19d3cda3a144b349d16dd3c81f7ca29cd9
Divesh Jadhav <diveshvjadhav@gmail.com>
2022-11-25 03:38:13 +0530

    Update student.js

--- pages/student.js
+++ pages/student.js
@@ -1,12 +1,86 @@
 import React from "react";
+import { useContext } from "react";
+import { DvanContext } from "../context/DvanContext";
 import Student from "../components/Student";
+import metamaskLogo from "../assets/metamask.png";
+import errorImg from "../assets/error.png";
+import Image from "next/image";
+
+const style = {
+  loginContainer: `w-full h-full flex flex-col justify-center items-center pb-48`,
+  walletConnectButton: `text-2xl text-black bg-white font-bold mb-[-3rem] mt-[3rem] px-6 py-4 rounded-full cursor-pointer hover:bg-[#d7dbdc]`,
+  loginContent: `text-3xl font-bold text-center mt-24`,
+};
 
 function student() {
-  return (
-    <div className="h-screen">
-      <Student />
+  const { appStatus, connectWallet } = useContext(DvanContext);
+
+  const app = (status = appStatus) => {
+    switch (status) {
+      case "connected":
+        return userLoggedIn;
+
+      case "notConnected":
+        return noUserFound;
+
+      case "noMetaMask":
+        return noMetaMaskFound;
+
+      case "error":
+        return error;
+
+      default:
+        return loading;
+    }
+  };
+
+  const userLoggedIn = <Student />;
+
+  const noUserFound = (
+    <div className={style.loginContainer}>
+      <Image src={metamaskLogo} width={200} height={200} />
+      <div
+        className={style.walletConnectButton}
+        onClick={() => connectWallet()}
+      >
+        Connect Wallet
+      </div>
+      <div className={style.loginContent}>Connect to Metamask.</div>
     </div>
   );
+
+  const noMetaMaskFound = (
+    <div className={style.loginContainer}>
+      <Image src={metamaskLogo} width={200} height={200} />
+      <div className={style.loginContent}>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href={`https://metamask.io/download.html`}
+        >
+          You must install Metamask, a <br /> virtual Ethereum wallet, in your
+          browser.
+        </a>
+      </div>
+    </div>
+  );
+
+  const error = (
+    <div className={style.loginContainer}>
+      <Image src={errorImg} width={250} height={200} />
+      <div className={style.loginContent}>
+        An error occurred. Please try again later or from another browser.
+      </div>
+    </div>
+  );
+
+  const loading = (
+    <div className={style.loginContainer}>
+      <div className={style.loginContent}>Loading...</div>
+    </div>
+  );
+
+  return <div>{app(appStatus)}</div>;
 }
 
 export default student;
